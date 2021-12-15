9f7dfedc3042ed4134cbc59ca3a3b6b021a3be96
Troitsky Maksim <horinf@mail.ru>
2021-12-15 09:51:44 +0300

    PWA, bootstrap, base app structure

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,10 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'tunes'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('tunes');
-  });
-
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('tunes app is running!');
+    expect(compiled.querySelector('.container span')?.textContent).toContain('My Tunes - the music');
   });
 });
